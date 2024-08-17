745b8286cf60e599ce9804485a828656872090e6
karonus <me@karonus.ru>
2024-08-17 22:57:12 +0300

    Fix commas in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import '../app.css';
-	import { ModeWatcher } from "mode-watcher";
+  import '../app.css';
+  import { ModeWatcher } from 'mode-watcher';
 </script>
 
 <ModeWatcher />
 
 <div class="container mx-auto">
-	<main class="mt-4">
-		<slot />
-	</main>
+  <main class="mt-4">
+    <slot />
+  </main>
 </div>
